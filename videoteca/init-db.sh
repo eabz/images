de934203e6a3fceccb1f7db2588bffd780ee0c9c
eabz <eabz@kindynos.mx>
2024-06-27 10:41:14 -0600

    Revert

--- videoteca/init-db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Change ownership of the PostgreSQL data directory
-chown -R postgres:postgres /var/lib/postgresql/data
-
-# Start the PostgreSQL server
-exec docker-entrypoint.sh postgres
